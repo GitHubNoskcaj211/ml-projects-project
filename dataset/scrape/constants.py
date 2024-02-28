dbeb5e0ef0a963bc9e3f97661f5456c6b179e0f8
Akash Munagala <akash.munagala@gmail.com>
2024-02-28 11:52:01 -0600

    Scrape free games

--- dataset/scrape/constants.py
+++ dataset/scrape/constants.py
@@ -24,7 +24,7 @@ USER_GAMES_FILENAME = os.path.join(SNOWBALL_ROOT_DIR, "users_games.csv")
 LOG_FILENAME = os.path.join(SNOWBALL_ROOT_DIR, "log.txt")
 
 FRIENDS_URL = f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={KEY}&steamid={{user_id}}&relationship=friend"
-GAMES_URL = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={KEY}&steamid={{user_id}}&include_appinfo=1&format=json"
+GAMES_URL = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={KEY}&steamid={{user_id}}&include_appinfo=1&include_played_free_games=1&format=json"
 GAME_DATA_URL = "https://api.gamalytic.com/game/{app_id}"
 
 
