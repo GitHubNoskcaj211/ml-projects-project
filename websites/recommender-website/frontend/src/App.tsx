57e819ae2c869b2ecc86c362c9d9840e67590a98
Akash Munagala <akash.munagala@gmail.com>
2024-03-05 00:26:31 -0600

    Commit frontend too

--- websites/recommender-website/frontend/src/App.tsx
+++ websites/recommender-website/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GameRating from "./GameRating";
 
 const App: React.FC = () => {
@@ -7,6 +7,35 @@ const App: React.FC = () => {
     { name: "CSGO 2", rating: 0.9 },
     { name: "Speedrunners", rating: 0.5 },
   ];
+
+  const [userID, setUserID] = useState<string | undefined | null>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("http://127.0.0.1:3000/user", {
+        mode: "cors",
+        credentials: "include",
+      });
+      if (res.status == 401) {
+        setUserID(null);
+        return;
+      }
+      const data = await res.json();
+      setUserID(data.id);
+    })();
+  }, []);
+
+  if (userID === undefined) {
+    return <div className="container">Loading...</div>;
+  }
+
+  if (userID === null) {
+    return (
+      <div className="container signInContainer">
+        <button onClick={() => location.href = "http://127.0.0.1:3000/login"}>Sign in through Steam</button>
+      </div>
+    );
+  }
 
   return (
     <div>
