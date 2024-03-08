74b5aa31db0ec6f36342ac6a37ee781e3a4ba57d
Akash Munagala <akash.munagala@gmail.com>
2024-03-07 23:45:54 -0600

    Fix user_exists call

--- backend/blueprints/recommendation.py
+++ backend/blueprints/recommendation.py
@@ -32,7 +32,7 @@ def get_recommendations(query: GetRecommendationFilterInput):
     data_loader = load_and_get_data_loader(current_app)
     model_wrapper = load_and_get_random_model_wrapper(current_app)
     model = model_wrapper.model
-    if not data_loader.user_exists(current_app, query.user_id):
+    if not data_loader.user_exists(query.user_id):
         return jsonify({"error": f"User with user_id {query.user_id} not found"}), 404
     recommendations = model.score_and_predict_n_games_for_user(
         query.user_id, query.N, should_sort=True
