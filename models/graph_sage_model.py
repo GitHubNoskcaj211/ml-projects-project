43835b35d2f274461817c92707ce9c6ae8d75bb0
Ninjasafd <JeffreyPan95@gmail.com>
2024-03-24 15:52:07 -0500

    Initial file creation

---
+++ models/graph_sage_model.py
@@ -0,0 +1,16 @@
+from models.base_model import BaseGameRecommendationModel
+import torch
+from torch_geometric.nn import SAGEConv
+
+
+class GraphSAGEModel(BaseGameRecommendationModel):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = SAGEConv(in_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv2(x, edge_index)
+        return x
