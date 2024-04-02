face5fe7849b6a966913b3249f7c5fe52e08c9e3
Akash Munagala <akash.munagala@gmail.com>
2024-04-02 04:58:50 -0500

    Start profiler in child processes

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,9 +1,12 @@
+import os
+
+
 def profiler_start():
-    print("Starting profiler")
+    print("Starting profiler", os.getpid())
     import googlecloudprofiler
     googlecloudprofiler.start(service="backend")
 
 
 def on_starting(server):
-    print("On starting")
     profiler_start()
+    os.register_at_fork(after_in_child=profiler_start)
