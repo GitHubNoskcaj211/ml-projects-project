e29618dc5fee647a7a66426b782fa083c390a15d
Akash Munagala <akash.munagala@gmail.com>
2024-04-02 05:17:05 -0500

    Start profiler on start too

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,5 +1,13 @@
 import googlecloudprofiler
 
 
-def post_fork(server, worker):
+def profiler_start():
     googlecloudprofiler.start(service="backend")
+
+
+def on_starting(server):
+    profiler_start()
+
+
+def post_fork(server, worker):
+    profiler_start()
