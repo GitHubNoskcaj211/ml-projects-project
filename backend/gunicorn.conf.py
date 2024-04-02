76e37d11f3fa9ab221cca7bf48c64af129b0b374
Akash Munagala <akash.munagala@gmail.com>
2024-04-02 05:03:26 -0500

    Only start profiler in workers

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,12 +1,7 @@
+import googlecloudprofiler
 import os
 
 
-def profiler_start():
+def post_fork(server, worker):
     print("Starting profiler", os.getpid())
-    import googlecloudprofiler
     googlecloudprofiler.start(service="backend")
-
-
-def on_starting(server):
-    profiler_start()
-    os.register_at_fork(after_in_child=profiler_start)
