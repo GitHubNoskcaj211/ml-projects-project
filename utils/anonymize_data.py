69402aa2a6a8e9bdc93783ea2da5a2960b840e76
Akash Munagala <akash.munagala@gmail.com>
2024-04-03 15:05:18 -0500

    Make snowball folders also anon

--- utils/anonymize_data.py
+++ utils/anonymize_data.py
@@ -19,6 +19,7 @@ next_anon_id = len(users)
 def get_anon_id(user_id):
     global next_anon_id
 
+    assert isinstance(user_id, int)
     id = users_mapping.get(user_id, None)
     if id is not None:
         return id
@@ -32,16 +33,18 @@ def anonymize_and_write(snowball, name, user_cols):
     df = pd.read_csv(os.path.join(ORIGINAL_ROOT, snowball, name))
     for col in user_cols:
         df[col] = df[col].map(get_anon_id)
-    df.to_csv(os.path.join(PUBLIC_ROOT, snowball, name), index=False)
+    anon_snowball = str(get_anon_id(int(snowball)))
+    df.to_csv(os.path.join(PUBLIC_ROOT, anon_snowball, name), index=False)
 
 
 snowballs = next(os.walk(os.path.join(ORIGINAL_ROOT)))[1]
 snowballs.append("")
 
 for snowball in snowballs:
-    if os.listdir(os.path.join(ORIGINAL_ROOT, snowball)) == []:
+    if not snowball.isdigit() or os.listdir(os.path.join(ORIGINAL_ROOT, snowball)) == []:
         continue
-    os.makedirs(os.path.join(PUBLIC_ROOT, snowball), exist_ok=True)
+    anon_snowball = str(get_anon_id(int(snowball)))
+    os.makedirs(os.path.join(PUBLIC_ROOT, anon_snowball), exist_ok=True)
     anonymize_and_write(snowball, "friends.csv", ["user1", "user2"])
     anonymize_and_write(snowball, "users_games.csv", ["user_id"])
     anonymize_and_write(snowball, "games.csv", [])
