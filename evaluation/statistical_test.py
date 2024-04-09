1d18e9fe53b4c2aa04d35229986bc0097f00c52a
Jackson Rusch <jackson.p.rusch@vanderbilt.edu>
2024-04-09 13:49:34 -0500

    Add the simulation statistical test.

---
+++ evaluation/statistical_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+def generator_for_normal_distribution(mean, variance):
+    return lambda: np.random.normal(mean, np.sqrt(variance))
+
+def estimate_probability_each_model_is_best(result_dataframe, metric, num_samples=100000, optimization_type='max'):
+    means = result_dataframe.loc[(result_dataframe[metric].notna()) & (result_dataframe[f'{metric}_variance'].notna()), metric].tolist()
+    variances = result_dataframe.loc[(result_dataframe[metric].notna()) & (result_dataframe[f'{metric}_variance'].notna()), f'{metric}_variance'].tolist()
+
+    if optimization_type == 'max':
+        optimization_function = np.argmax
+    elif optimization_type == 'min':
+        optimization_function = np.argmin
+    else:
+        raise Exception('Choose valid optimization type.')
+
+    best_counts = [0] * len(means)
+    generators = [generator_for_normal_distribution(mean, variance) for mean, variance in zip(means, variances)]
+    for ii in range(num_samples):
+        generated_values = [generator() for generator in generators]
+        best_counts[optimization_function(generated_values)] += 1
+
+    probabilities = [cc / num_samples for cc in best_counts]
+    result_dataframe.loc[(result_dataframe[metric].notna()) & (result_dataframe[f'{metric}_variance'].notna()), f'{metric}_best_probability'] = probabilities
