523fe55c8fed4775d1d49c8f8fa69d5039f39b03
Ninjasafd <JeffreyPan95@gmail.com>
2024-04-18 15:24:21 -0500

    Hopefully text changes

--- websites/recommender-website/src/components/NavBar.tsx
+++ websites/recommender-website/src/components/NavBar.tsx
@@ -11,16 +11,16 @@ const Navbar: React.FC<NavbarProps> = ({ setCurrentView }) => {
     return (
         <div className="flex justify-between items-center px-4 py-2 bg-gray-800 text-white">
             <div className="flex-grow">
-                <button className="text-lg navbar-btn md:text-sm  sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("HomePage")}>
+                <button className="text-lg navbar-btn md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("HomePage")}>
                     Home Page
                 </button>
-                <button className="text-lg md:text-sm sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("FindNewGames")}>
+                <button className="text-lg md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("FindNewGames")}>
                     Find New Games
                 </button>
-                <button className="text-lg md:text-sm sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("LikedGames")}>
+                <button className="text-lg md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("LikedGames")}>
                     Liked Games
                 </button>
-                <button className="text-lg md:text-sm sm:text-sm mr-2 sm:mr-4" onClick={() => window.open("mailto:jackson.p.rusch@vanderbilt.edu", "_blank")}>
+                <button className="text-lg md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => window.open("mailto:jackson.p.rusch@vanderbilt.edu", "_blank")}>
                     Feedback
                 </button>
             </div>
