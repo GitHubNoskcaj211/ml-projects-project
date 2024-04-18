ad2f915166855ee33aad00b86df250e5710fa1fa
Ninjasafd <JeffreyPan95@gmail.com>
2024-04-18 13:27:12 -0500

    Text changes dont seem to matter...

--- websites/recommender-website/src/components/PopUpBox.tsx
+++ websites/recommender-website/src/components/PopUpBox.tsx
@@ -17,9 +17,9 @@ const PopUpBox: React.FC<PopUpBoxProps> = ({ isOpen, onClose }) => {
             <div className="popup-inner bg-white p-4 rounded-md shadow-lg max-w-full sm:max-w-lg mx-auto">
                 <button className="popup-close-btn absolute top-2 right-2" onClick={onClose}>X</button>
                 <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-3">Steam Recommendations</h1>
-                <p className="text-lg mb-3">Get personalized game recommendations! {isMobile ? "Swipe right" : "Click the right arrow"} if you like a game, or {isMobile ? "swipe left" : "click the left arrow"} if it's not your cup of tea. </p>
+                <p className="text-lg sm:text-sm mb-3">Get personalized game recommendations! {isMobile ? "Swipe right" : "Click the right arrow"} if you like a game, or {isMobile ? "swipe left" : "click the left arrow"} if it's not your cup of tea. </p>
 
-                <p className="text-lg mb-3">Start by exploring the game below and select your preferences to get recommendations tailored just for you.</p>
+                <p className="text-lg sm:text-sm mb-3">Start by exploring the game below and select your preferences to get recommendations tailored just for you.</p>
 
                 {isMobile ? (
                     <>
