37ddbeb474ef5b99951e6aab2448c83762a00154
Ninjasafd <JeffreyPan95@gmail.com>
2024-04-18 13:04:45 -0500

    NavBar Update

--- websites/recommender-website/src/components/NavBar.tsx
+++ websites/recommender-website/src/components/NavBar.tsx
@@ -11,21 +11,21 @@ const Navbar: React.FC<NavbarProps> = ({ setCurrentView }) => {
     return (
         <div className="flex justify-between items-center px-4 py-2 bg-gray-800 text-white">
             <div className="flex-grow">
-                <button className="text-xl navbar-btn md:text-lg  sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("HomePage")}>
+                <button className="text-lg navbar-btn md:text-sm  sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("HomePage")}>
                     Home Page
                 </button>
-                <button className="text-xl md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("FindNewGames")}>
+                <button className="text-lg md:text-sm sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("FindNewGames")}>
                     Find New Games
                 </button>
-                <button className="text-xl md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("LikedGames")}>
+                <button className="text-lg md:text-sm sm:text-sm mr-2 sm:mr-4" onClick={() => setCurrentView("LikedGames")}>
                     Liked Games
                 </button>
-                <button className="text-xl md:text-lg sm:text-sm mr-2 sm:mr-4" onClick={() => window.open("mailto:jackson.p.rusch@vanderbilt.edu", "_blank")}>
+                <button className="text-lg md:text-sm sm:text-sm mr-2 sm:mr-4" onClick={() => window.open("mailto:jackson.p.rusch@vanderbilt.edu", "_blank")}>
                     Feedback
                 </button>
             </div>
             <div>
-                <button className="text-s sm:text-sm justify-right" onClick={() => window.location.replace(makeBackendURL("logout"))}>
+                <button className="text-lg sm:text-sm justify-right" onClick={() => window.location.replace(makeBackendURL("logout"))}>
                     Logout
                 </button>
             </div>
