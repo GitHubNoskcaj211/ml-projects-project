8f0dea3626bd56d1de97db7a523de286456dc74e
Akash Munagala <akash.munagala@gmail.com>
2024-06-20 16:18:46 -0500

    Reformat

--- websites/recommender-website/src/components/NavBar.tsx
+++ websites/recommender-website/src/components/NavBar.tsx
@@ -1,43 +1,59 @@
 // Navbar.tsx
 import React from "react";
-import { makeBackendURL } from "../util";
-import './NavBar.css';
+import "./NavBar.css";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
 export enum NavbarView {
-    LandingPage,
-    FindNewGames,
-    Interactions,
-    HomePage,
-  }
+  LandingPage,
+  FindNewGames,
+  Interactions,
+  HomePage,
+}
 
 interface NavbarProps {
-    setCurrentView: React.Dispatch<React.SetStateAction<NavbarView>>;
+  setCurrentView: React.Dispatch<React.SetStateAction<NavbarView>>;
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ setCurrentView }) => {
-    return (
-        <div className="flex justify-between items-center px-4 py-2 bg-gray-800 text-white">
-            <div className="flex-grow">
-                <button className="text-sm navbar-btn md:text-lg mr-2 sm:mr-4" onClick={() => setCurrentView(NavbarView.HomePage)}>
-                    Home Page
-                </button>
-                <button className="text-sm md:text-lg mr-2 sm:mr-4" onClick={() => setCurrentView(NavbarView.FindNewGames)}>
-                    Find New Games
-                </button>
-                <button className="text-sm md:text-lg mr-2 sm:mr-4" onClick={() => setCurrentView(NavbarView.Interactions)}>
-                    Interactions
-                </button>
-                <button className="text-sm md:text-lg mr-2 sm:mr-4" onClick={() => window.open("mailto:jackson.p.rusch@vanderbilt.edu", "_blank")}>
-                    Feedback
-                </button>
-            </div>
-            <div>
-                <button className="text-sm md:text-lg justify-right" onClick={() => signOut(auth)}>
-                    Logout
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center px-4 py-2 bg-gray-800 text-white">
+      <div className="flex-grow">
+        <button
+          className="text-sm navbar-btn md:text-lg mr-2 sm:mr-4"
+          onClick={() => setCurrentView(NavbarView.HomePage)}
+        >
+          Home Page
+        </button>
+        <button
+          className="text-sm md:text-lg mr-2 sm:mr-4"
+          onClick={() => setCurrentView(NavbarView.FindNewGames)}
+        >
+          Find New Games
+        </button>
+        <button
+          className="text-sm md:text-lg mr-2 sm:mr-4"
+          onClick={() => setCurrentView(NavbarView.Interactions)}
+        >
+          Interactions
+        </button>
+        <button
+          className="text-sm md:text-lg mr-2 sm:mr-4"
+          onClick={() =>
+            window.open("mailto:jackson.p.rusch@vanderbilt.edu", "_blank")
+          }
+        >
+          Feedback
+        </button>
+      </div>
+      <div>
+        <button
+          className="text-sm md:text-lg justify-right"
+          onClick={() => signOut(auth)}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
